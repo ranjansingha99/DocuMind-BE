a223c0b04585b9b48d2ad1e806cd4ac6b2293430
Ranjan Kumar Singha <ranjansingha.coding@gmail.com>
2026-02-01 20:16:46 +0530

    SpringBoot init

---
+++ src/test/java/com/documind/docuMind/DocuMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.documind.docuMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
